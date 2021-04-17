61b0230c338b5326033d210e1a17377d6f390f28
hashim <hashimtahir@live.com>
2021-04-17 15:53:17 +0500

    Difference between Hilt and dagger 2. And their initilizations.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/hiltdependencyinjection/BaseApplication.kt
+++ app/src/main/java/com/hashim/hiltdependencyinjection/BaseApplication.kt
@@ -11,7 +11,21 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 /*Base application class is a requirement for hilt to work it must
-* be annotated with HiltAndroidApp */
+* be annotated with HiltAndroidApp
+*
+* @HiltAndroidApp triggers Hilt's code generation, including a base class for the application
+* that serves as the application-level dependency container.
+*
+* Unlike dagger 2 hilt does this by iteself and dosent require any effort on developer's part,
+* In dagger 2, One has to build the application class, initilize dagger 2,
+* Build the appcompnets, build modules for the components, Use the builder or factory pattern
+* to build the components. Save the refrence fo componets in the application class. Rebuild the
+* project. which then just initilizes dagger 2.
+*
+*
+* None of  this is required in Hilt which does it all behind the scenes, it also removes all
+*  the boilerplate code one has to write , just to setup dagger 2  as explained above.
+* */
 @HiltAndroidApp
 class BaseApplication : Application() {
     override fun onCreate() {
