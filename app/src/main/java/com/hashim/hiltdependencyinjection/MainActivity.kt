4175dfa715befb09c08d74e256c522fce88a9150
hashim <hashimtahir@live.com>
2021-04-17 13:24:41 +0500

    Initial commit

---
+++ app/src/main/java/com/hashim/hiltdependencyinjection/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021/  4/ 17.  Created by Hashim Tahir
+ */
+
+package com.hashim.hiltdependencyinjection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
